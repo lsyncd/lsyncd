c09dcd1775621fc26374cec0d4cb9acc2b3c477c
Axel Kittenberger <axkibe@gmail.com>
2008-11-05 18:56:24 +0000

    for older boxes this can be useful to pick the right automake binary.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # simplistic script to run after checkout
 
-aclocal && \
+# In case of explicit having a 1.7 version use that (for systems having multiple automake versions installed)
+ACLOCAL=`which aclocal-1.7 || echo aclocal`
+AUTOMAKE=`which automake-1.7 || echo automake`
+
+$ACLOCAL && \
 autoheader && \
 autoconf && \
-automake -a -c
+$AUTOMAKE -a -c
