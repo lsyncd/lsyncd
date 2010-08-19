9ed9df5effe9638befb5840e8bd9b8f3a9291e20
Axel Kittenberger <axkibe@gmail.com>
2010-08-19 13:30:18 +0000

    added -s (protect args) to default rsync)

--- lsyncd.c
+++ lsyncd.c
@@ -338,7 +338,7 @@ struct global_options {
  * Standard default options to call the binary with.
  */
 struct call_option standard_callopts[] = {
-	{ CO_TEXT,    "-lt%r"    },
+	{ CO_TEXT,    "-lts%r"    },
 	{ CO_TEXT,    "--delete" },
 	{ CO_EXCLUDE, NULL       },
 	{ CO_FILTER,  NULL       },
