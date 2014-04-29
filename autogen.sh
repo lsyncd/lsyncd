f199fd1866c347d895b736aae345dde4aca11c2c
Axel Kittenberger <axkibe@gmail.com>
2014-04-29 16:19:18 +0200

    this can now also go

--- autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Generating configure files... may take a while."
-
-autoreconf --install --force && \
-  echo "Preparing was successful if there was no error messages above." && \
-  echo "Now type:" && \
-  echo "  ./configure && make"  && \
-  echo "Run './configure --help' for more information"
