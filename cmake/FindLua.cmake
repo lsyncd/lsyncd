465deb15490355efdb713f13f22f9ed32b961515
Axel Kittenberger <axkibe@gmail.com>
2018-03-09 13:11:16 +0100

    prefer Lua 5.2 over 5.3 (due to second missing supportlibs like posix on debian)

--- cmake/FindLua.cmake
+++ cmake/FindLua.cmake
@@ -36,7 +36,7 @@ SET(_POSSIBLE_LUA_INCLUDE include include/lua)
 #SET(_POSSIBLE_LUA_LIBRARY lua)
 
 # Determine possible naming suffixes (there is no standard for this)
-SET(_POSSIBLE_SUFFIXES "53" "5.3" "-5.3" "52" "5.2" "-5.2" "")
+SET(_POSSIBLE_SUFFIXES "52" "5.2" "-5.2" "53" "5.3" "-5.3" "")
 
 # Set up possible search names and locations
 FOREACH(_SUFFIX ${_POSSIBLE_SUFFIXES})
