938c702525b5c53b8742928699452566b5547e24
Daniel Poelzleithner <git@poelzi.org>
2021-12-10 12:17:54 +0100

    cleanup ssh server when aborting ci run

--- tests/ci-run.sh
+++ tests/ci-run.sh
@@ -2,6 +2,15 @@
 
 set -ex
 
+function cleanup() {
+        echo "** abort. cleanup ssh server"
+        cd `dirname $BASH_SOURCE`/..
+        # CLEANUP=`dirname $BASH_SOURCE`/../teardown.lua
+        lua tests/teardown.lua
+}
+
+trap cleanup INT EXIT
+
 SRC=`pwd`
 BUILD_FOLDER=`mktemp -d`
 echo "Build folder: $BUILD_FOLDER"
