24ef43f5fd08046e797af19e3d628679ddce796d
Daniel Poelzleithner <poelzleithner@b1-systems.de>
2022-06-03 06:43:43 +0200

    Add lua-crontab to dependencies

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Lsyncd (Live Syncing Daemon)";
   
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -28,11 +28,11 @@
             (builtins.match ''.*set\(.LSYNCD_VERSION ([0-9\.]*).*''
               (builtins.substring 0 500
                 (builtins.readFile ./CMakeLists.txt))) 0;
-          mylua5_4 = pkgs.lua5_4.override({
-            packageOverrides =  luaself: luaprev: {
-              luarocks = luaprev.luarocks-3_7;
-            };
-          });
+          # mylua5_4 = pkgs.lua5_4.override({
+          #   packageOverrides =  luaself: luaprev: {
+          #     luarocks = luaprev.luarocks-3_7;
+          #   };
+          # });
           luaposix35 = mylua: mylua.pkgs.buildLuarocksPackage {
             pname = "luaposix";
             lua = mylua;
@@ -56,15 +56,82 @@
             };
           };
 
+          buildExtensions = luapkgs: (
+            let
+              nucleo = luapkgs.buildLuarocksPackage {
+                pname = "lua-nucleo";
+                version = "1.1.0-1";
+                knownRockspec = (pkgs.fetchurl {
+                  url    = "https://luarocks.org/lua-nucleo-1.1.0-1.rockspec";
+                  sha256 = "02ly51wav1pxiahf6lflr4vks550bisdq4ir9cy1lxn9v2zmcbim";
+                }).outPath;
+                src = pkgs.fetchgit ( removeAttrs (builtins.fromJSON ''{
+                "url": "https://github.com/lua-nucleo/lua-nucleo.git",
+                "rev": "76835968ff30f182367abd58637560402990e0b1",
+                "date": "2021-04-26T11:51:34+03:00",
+                "path": "/nix/store/3ycmrh0j64qxm4f04yxmn3y42imc8bv5-lua-nucleo",
+                "sha256": "15kydmj64jhxv5ksayfgkwzmgzd7raj7xp636x8a7c3ybiirs90n",
+                "fetchSubmodules": true,
+                "deepClone": false,
+                "leaveDotGit": false
+              }
+              '') ["date" "path"]) ;
+
+                disabled = with luapkgs; (luaOlder "5.1");
+
+                meta = {
+                  homepage = "http://github.com/lua-nucleo/lua-nucleo";
+                  description = "A random collection of core and utility level Lua libraries";
+                  license.fullName = "MIT/X11";
+                };
+              };
+            in
+              luapkgs.buildLuarocksPackage {
+                pname = "lua-crontab";
+                version = "1.0.0-1";
+                knownRockspec = (pkgs.fetchurl {
+                  url    = "https://luarocks.org/lua-crontab-1.0.0-1.rockspec";
+                  sha256 = "1aynwxq488sxd2lyng4wnswfkqna5n07sfmdainlqlhcb6jan161";
+                }).outPath;
+                src = pkgs.fetchgit ( removeAttrs (builtins.fromJSON ''{
+                "url": "https://github.com/logiceditor-com/lua-crontab.git",
+                "rev": "e3929a572e8164f968da4dcbdf1c4464a2870699",
+                "date": "2021-07-29T14:12:08+03:00",
+                "path": "/nix/store/rsc49m4f1mjqbffaq7axcf31rgxxfjb3-lua-crontab",
+                "sha256": "0zkqslw3vg495k8g010cz931vlzfyynq4kcwi1jbbppia521z6rx",
+                "fetchSubmodules": true,
+                "deepClone": false,
+                "leaveDotGit": false
+              }
+              '') ["date" "path"]) ;
+
+                propagatedBuildInputs = [ nucleo ];
+
+                meta = {
+                  homepage = "http://github.com/logiceditor-com/lua-crontab";
+                  description = "Stores crontab-like rules for events and calculates timestamps for their occurrences";
+                  license.fullName = "MIT/X11";
+                };
+              }
+          );
+
           buildTypes = { 
-            lua5_1 = [pkgs.lua5_1 pkgs.lua51Packages.luaposix];
-            lua5_2 = [pkgs.lua5_2 pkgs.lua52Packages.luaposix];
-            lua5_3 = [pkgs.lua5_3 pkgs.lua53Packages.luaposix];
-            lua5_4 = [pkgs.lua5_3 (luaposix35 mylua5_4)];
+            lua5_1 = [pkgs.lua5_1 pkgs.lua51Packages.luaposix (buildExtensions pkgs.lua51Packages)];
+            lua5_2 = [pkgs.lua5_2 pkgs.lua52Packages.luaposix (buildExtensions pkgs.lua52Packages)];
+            lua5_3 = [pkgs.lua5_3 pkgs.lua53Packages.luaposix (buildExtensions pkgs.lua53Packages)];
+            # lua5_4 = [mylua5_4pkgs.lua5_3 (luaposix35 mylua5_4)];
+            lua5_4 = [pkgs.lua5_4 (luaposix35 pkgs.lua5_4)  (buildExtensions pkgs.lua5_4.pkgs)];
           };
+
+          # buildTypes = {
+          #   lua5_1 = [(pkgs.lua5_1.withPackages (ps: [ps.luaposix (buildExtensions pkgs.lua51Packages)]))];
+          #   lua5_2 = [(pkgs.lua5_2.withPackages (ps: [ps.luaposix (buildExtensions pkgs.lua52Packages)]))];
+          #   lua5_3 = [(pkgs.lua5_3.withPackages (ps: [ps.luaposix (buildExtensions pkgs.lua53Packages)]))];
+          #   lua5_4 = [(pkgs.lua5_4.withPackages (ps: [ps.luaposix (buildExtensions pkgs.lua54Packages)]))];
+          # };
         in
         let
-           mkLsync = luaPackages: pkgs.stdenv.mkDerivation ({
+          mkLsync = luaPackages: pkgs.stdenv.mkDerivation ({
             inherit version;
             name = "lsyncd";
 
@@ -76,8 +143,8 @@
 
             buildInputs = defaultDeps ++ luaPackages;
           });
-          mkDev = packages: pkgs.mkShell {
-            propagatedBuildInputs = defaultDeps ++ packages;
+          mkDev = extras: pkgs.mkShell {
+            propagatedBuildInputs = defaultDeps ++ extras;
           };
         in
         {
